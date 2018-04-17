64c1404960c3116708d179da26508dde45693dbb
Mateusz Krainski <Mateusz Krainski@esa-ad.esa.int>
2018-04-17 13:40:38 +0200

    Draft version working

--- generate_report.py
+++ generate_report.py
@@ -1,10 +1,52 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--filename",
+    help="ERICA's log file to be parsed. If empty, will use the most recent.",
+    required=True)
+parser.add_argument("-d", "--description",
+    default="--",
+    help="This is the description of this test, "
+         "it will be included in the report")
+
+script_arguments = parser.parse_args()
+
 import nbparameterise
 import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+import subprocess
+import sys
+APPNAME = 'create_report'
 
-with open("Untitled.ipynb") as f:
+with open("report_template.ipynb") as f:
     nb = nbformat.read(f, as_version=4)
 
 
-params = nbparameterise.extract_parameters(nb)
+orig_parameters = nbparameterise.extract_parameters(nb)
 
-print(params)
+ep = ExecutePreprocessor(timeout=600, kernel_name='reportTool')
+
+params = nbparameterise.parameter_values(
+    orig_parameters,
+    testfile_name=script_arguments.filename,
+    description=script_arguments.description)
+
+new_nb = nbparameterise.replace_definitions(nb, params, execute=False)
+
+_ = ep.preprocess(new_nb, {'metadata': {'path': '.'}})
+RESULT_FILENAME = "_report.ipynb"
+with open(RESULT_FILENAME, 'wt') as f:
+    nbformat.write(new_nb, f)
+
+try:
+    # Convert to html
+    print('[{}] Converting to html'.format(APPNAME))
+    subprocess.call(['jupyter', 'nbconvert', RESULT_FILENAME, '--to', 'html'])
+
+finally:
+    # Delete unnecessary notebook file
+    print('[{}] Deleting {}'.format(APPNAME, RESULT_FILENAME))
+    if sys.platform == 'win32':
+        subprocess.call(['del', RESULT_FILENAME], shell=True)
+    else:
+        subprocess.call(['rm {}'.format(RESULT_FILENAME)], shell=True)
